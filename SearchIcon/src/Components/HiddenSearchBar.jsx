8a50f6ff34f21a4717b2d12ea30444b5a3090850
martinristow <martinristov084@gmail.com>
2025-04-06 16:24:39 +0200

    create hidden search bar component
    - Added HiddenSearchBar component
    - Toggle input field visibility on search icon click
    - Change background color on focus and blur
    - Used react-icons (FaSearch) for the search icon
    - Applied conditional rendering and inline styling

--- SearchIcon/src/Components/HiddenSearchBar.jsx
+++ SearchIcon/src/Components/HiddenSearchBar.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import {useState} from 'react'
+import '../style.css'
+import {FaSearch} from 'react-icons/fa'
 
 const HiddenSearchBar = () => {
+
+    const [showInput, setShowInput] = useState(false)
+    const [bgColor, setBgColor] = useState('white')
+
+    const handleClick = (e) => {
+        setBgColor('#1a1a1a')
+        if (e.target.className === 'container') {
+            setShowInput(false)
+            setBgColor('#fff')
+        }
+    }
+
     return (
-        <div>HiddenSearchBar</div>
+        <section className='container' style={{backgroundColor: bgColor}} onClick={handleClick}>
+            {showInput ? (
+                <input type='text' placeholder='Search...'/>
+            ) : (
+                <FaSearch onClick={() => setShowInput(true)}/>
+            )}
+        </section>
     )
 }
 export default HiddenSearchBar
