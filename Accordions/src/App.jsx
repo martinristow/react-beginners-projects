da1565ae3edc491663c9988c2e09c4bbea0888a0
martinristow <martinristov084@gmail.com>
2025-04-06 21:58:39 +0200

    Initial Commit

---
+++ Accordions/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Accordion from "./Components/Accordion.jsx";
+
+const App = () => {
+    return (
+        <div>
+            <Accordion/>
+        </div>
+    )
+}
+export default App
---
+++ Accordions/src/Components/Accordion.jsx
@@ -0,0 +1,10 @@
+import '../style.css'
+
+const Accordion = () => {
+    return (
+        <div>
+            <h1>Hi</h1>
+        </div>
+    )
+}
+export default Accordion
