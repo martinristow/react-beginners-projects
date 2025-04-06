da39f9521226de662e0a7ed81a88c52d64174f0a
martinristow <martinristov084@gmail.com>
2025-04-06 16:32:13 +0200

    Initial Commit

---
+++ Testamonails/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Testimonials from "./Components/Testimonials.jsx";
+
+const App = () => {
+    return (
+        <div>
+            <Testimonials/>
+        </div>
+    )
+}
+export default App
