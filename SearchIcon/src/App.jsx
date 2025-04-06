79dc6f40a9b4b2ff8908f2c9332cdafa3886e4af
martinristow <martinristov084@gmail.com>
2025-04-06 16:16:05 +0200

    Initial Commit

---
+++ SearchIcon/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import HiddenSearchBar from "./Components/HiddenSearchBar.jsx";
+
+const App = () => {
+    return (
+        <div>
+            <HiddenSearchBar />
+        </div>
+    )
+}
+export default App
