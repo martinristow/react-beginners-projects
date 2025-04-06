3914303463687cf91a141aa7c65561b1b2cc18ae
martinristow <martinristov084@gmail.com>
2025-04-06 17:29:48 +0200

    Refactor Testimonials component: Move testimonials data to separate file
    - Moved the testimonials data to a new file: `Utils/Testimonials.js`.
    - Updated the `Testimonials` component to import the testimonials data from the new file.
    - The component still supports cycling through the testimonials using "Prev" and "Next" buttons.

--- Testamonails/src/Components/Testimonials.jsx
+++ Testamonails/src/Components/Testimonials.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import {useState} from 'react'
+import '../style.css'
+import {testimonials} from "../Utils/Testimonials.js";
 
 const Testimonials = () => {
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+    
+    const handlePrevClick = () => {
+        setCurrentIndex((currentIndex + testimonials.length - 1) % testimonials.length);
+    }
+    const handleNextClick = () => {
+        setCurrentIndex((currentIndex + testimonials.length + 1) % testimonials.length)
+    }
+
     return (
-        <div>Testimonials</div>
+        <div className='testimonials'>
+            <div className='testimonials-quote'>
+                {testimonials[currentIndex].quote}
+            </div>
+
+            <div className='testimonials-author'>
+                - {testimonials[currentIndex].author}
+            </div>
+
+            <div className="testimonials-nav">
+                <button onClick={handlePrevClick}>Prev</button>
+                <button onClick={handleNextClick}>Next</button>
+            </div>
+
+        </div>
     )
 }
 export default Testimonials
