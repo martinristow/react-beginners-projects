d9f60dce1b83601601b2fd3029659b96ad0496b8
martinristow <martinristov084@gmail.com>
2025-04-06 17:29:18 +0200

    Dummy Data for Testimonials

---
+++ Testamonails/src/Utils/Testimonials.js
@@ -0,0 +1,14 @@
+export const testimonials = [
+    {
+        quote: "This is the best product I've ever used!",
+        author: "Jane Doe",
+    },
+    {
+        quote: "I highly recommend this product to everyone!",
+        author: "John Smith",
+    },
+    {
+        quote: "This product has completely changed my life!",
+        author: "Bob Johnson",
+    },
+];
