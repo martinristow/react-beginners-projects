bb94e0f2a8bb6866f1a0f056b0e157fd7442ef31
martinristow <martinristov084@gmail.com>
2025-04-06 15:25:25 +0200

    Add interactive calculator functionality using eval
    - Implemented `display()` function to append input values
    - Connected all number and operator buttons to update input dynamically
    - Added `calculate()` function to evaluate the expression using eval
    - Fixed clear button to correctly reset the input field
    - Updated button event handlers with onClick functions

--- Calculator/src/Components/Calculator.jsx
+++ Calculator/src/Components/Calculator.jsx
@@ -5,30 +5,35 @@ const Calculator = () => {
 
     const [inputValue, setInputValue] = useState('');
 
+    const display = (value) => setInputValue(inputValue + value);
+    const calculate = () => setInputValue(eval(inputValue));
+
     const clear = () => setInputValue('');
 
     return (
         <form className='calculator' name='calc'>
             <input type="text" className="value" value={inputValue}/>
 
-            <span className="num clear" onClick={() => clear}>c</span>
+            {/*<span className="num clear" onClick={clear}>c</span>*/}
+            <span className="num clear" onClick={() => clear()}>c</span>
 
-            <span>/</span>
-            <span>*</span>
-            <span>7</span>
-            <span>8</span>
-            <span>9</span>
-            <span>-</span>
-            <span>4</span>
-            <span>5</span>
-            <span>6</span>
-            <span>+</span>
-            <span>1</span>
-            <span>2</span>
-            <span>3</span>
-            <span>0</span>
-            <span>.</span>
-            <span>=</span>
+            <span onClick={() => display('/')}>/</span>
+            <span onClick={() => display('*')}>*</span>
+            <span onClick={() => display('7')}>7</span>
+            <span onClick={() => display('8')}>8</span>
+            <span onClick={() => display('9')}>9</span>
+            <span onClick={() => display('-')}>-</span>
+            <span onClick={() => display('4')}>4</span>
+            <span onClick={() => display('5')}>5</span>
+            <span onClick={() => display('6')}>6</span>
+            <span className="plus" onClick={()=> display('+')}>+</span>
+
+            <span onClick={()=> display('1')}>1</span>
+            <span onClick={()=> display('2')}>2</span>
+            <span onClick={()=> display('3')}>3</span>
+            <span onClick={()=> display('0')}>0</span>
+            <span onClick={()=> display('.')}>.</span>
+            <span className='num equal' onClick={()=> calculate()}>=</span>
 
         </form>
     )
