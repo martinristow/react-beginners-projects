73f36733f85113bc9df4234d8ac98cf5716b6742
martinristow <martinristov084@gmail.com>
2025-04-07 20:06:00 +0200

    Refactor Price.jsx to use Input component for price range selection
    - Replaced static radio buttons with Input component for price range selection
    - Passed `handleChange` function to Input component for managing state
    - Updated price values for different ranges in Input components

--- E-Commerce-Advance-Filtering/src/SideBar/Price/Price.jsx
+++ E-Commerce-Advance-Filtering/src/SideBar/Price/Price.jsx
@@ -1,35 +1,44 @@
 import React from 'react'
 import './Price.css'
+import Input from "../../Components/Input.jsx";
 
-const Price = () => {
+const Price = ({handleChange}) => {
+
     return (
         <div className='ml'>
             <h2 className="sidebar-title price-title">Price</h2>
-
             <label className="sidebar-label-container">
-                <input type="radio" name="test2"/>
+                <input onChange={handleChange} type="radio" value='' name='test2'/>
                 <span className="checkmark"></span>All
             </label>
 
-            <label className="sidebar-label-container">
-                <input type="radio" name="test2"/>
-                <span className="checkmark"></span>$0 - $50
-            </label>
+            <Input
+            handleChange={handleChange}
+            value={50}
+            title='$0 - 50'
+            name='test2'
+            />
 
-            <label className="sidebar-label-container">
-                <input type="radio" name="test2"/>
-                <span className="checkmark"></span>$50 - $100
-            </label>
+            <Input
+                handleChange={handleChange}
+                value={100}
+                title='$50 - 100'
+                name='test2'
+            />
 
-            <label className="sidebar-label-container">
-                <input type="radio" name="test2"/>
-                <span className="checkmark"></span>$100 - $150
-            </label>
+            <Input
+                handleChange={handleChange}
+                value={150}
+                title='$100 - 150'
+                name='test2'
+            />
 
-            <label className="sidebar-label-container">
-                <input type="radio" name="test2"/>
-                <span className="checkmark"></span>Over $150
-            </label>
+            <Input
+                handleChange={handleChange}
+                value={200}
+                title='$Over - 150'
+                name='test2'
+            />
         </div>
     )
 }
