7adca892d897d3f415d6f7b4dfcede479550a8cf
martinristow <martinristov084@gmail.com>
2025-04-07 20:03:31 +0200

    Refactor Products.jsx to dynamically render cards based on result prop
    - Replaced static product data with dynamic `result` prop to render product cards
    - Integrated the Card component to display filtered products

--- E-Commerce-Advance-Filtering/src/Products/Products.jsx
+++ E-Commerce-Advance-Filtering/src/Products/Products.jsx
@@ -1,37 +1,11 @@
 import './Products.css'
-import {AiFillStar} from "react-icons/ai";
-import {BsFillBagHeartFill} from "react-icons/bs";
+import Card from "../Components/Card.jsx";
 
-const Products = () => {
+const Products = ({result}) => {
     return (
         <>
             <section className='card-container'>
-                <section className="card">
-                    <img className='card-image' src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
-                         alt="Shoe"/>
-                    <div className="card-details">
-                        <h3 className="card-title">Shoe</h3>
-                        <section className="card-reviews">
-                            <AiFillStar className='ratings-star'/>
-                            <AiFillStar className='ratings-star'/>
-                            <AiFillStar className='ratings-star'/>
-                            <AiFillStar className='ratings-star'/>
-                            <span className="total-reviews">5</span>
-                        </section>
-                        <section className="card-price">
-                            <div className='price'>
-                                <del>$300</del>
-                                $200
-                            </div>
-
-                            <div className='bag'>
-                                <BsFillBagHeartFill className='bag-icon'/>
-                            </div>
-
-                        </section>
-
-                    </div>
-                </section>
+                {result}
             </section>
         </>
     )
