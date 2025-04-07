0b5d1b70d7e73b05f075850d5160a3bfc92eeb7d
martinristow <martinristov084@gmail.com>
2025-04-07 19:59:40 +0200

    Refactor Buttons.jsx to handle dynamic button rendering with click functionality
    - Modified Buttons.jsx to accept `onClickHandler`, `value`, and `title` as props for dynamic button rendering
    - Added event handling for button clicks to trigger specific actions

---
+++ E-Commerce-Advance-Filtering/src/Components/Buttons.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Buttons = ({onClickHandler, value, title}) => {
+    return (
+        <button onClick={onClickHandler} value={value} className='btns'>{title}</button>
+    )
+}
+export default Buttons
