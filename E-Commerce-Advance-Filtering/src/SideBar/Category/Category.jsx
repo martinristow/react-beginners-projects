5f14ecdb25cabfc6cd14c383b3c73e89a4491afa
martinristow <martinristov084@gmail.com>
2025-04-07 20:04:49 +0200

    Refactor Category.jsx to use Input component for dynamic category selection
    - Replaced static radio buttons with Input component for category selection
    - Passed `handleChange` function to Input component for managing state

--- E-Commerce-Advance-Filtering/src/SideBar/Category/Category.jsx
+++ E-Commerce-Advance-Filtering/src/SideBar/Category/Category.jsx
@@ -1,32 +1,44 @@
 import React from 'react'
 import './Category.css'
+import Input from "../../Components/Input.jsx";
 
-const Category = () => {
+const Category = ({handleChange}) => {
     return (
         <div>
             <h2 className="sidebar-title">Category</h2>
 
             <div>
                 <label className="sidebar-label-container">
-                    <input type="radio" name="test"/>
+                    <input onChange={handleChange} type="radio" value='' name='test'/>
                     <span className="checkmark"></span>All
                 </label>
-                <label className="sidebar-label-container">
-                    <input type="radio" name="test"/>
-                    <span className="checkmark"></span>Sneakers
-                </label>
-                <label className="sidebar-label-container">
-                    <input type="radio" name="test"/>
-                    <span className="checkmark"></span>Flats
-                </label>
-                <label className="sidebar-label-container">
-                    <input type="radio" name="test"/>
-                    <span className="checkmark"></span>Sandals
-                </label>
-                <label className="sidebar-label-container">
-                    <input type="radio" name="test"/>
-                    <span className="checkmark"></span>Heels
-                </label>
+                <Input
+                    handleChange={handleChange}
+                    value='sneakers'
+                    title='Sneakers'
+                    name='test'
+                />
+
+                <Input
+                    handleChange={handleChange}
+                    value='flats'
+                    title='Flats'
+                    name='test'
+                />
+
+                <Input
+                    handleChange={handleChange}
+                    value='sandals'
+                    title='Sandals'
+                    name='test'
+                />
+
+                <Input
+                    handleChange={handleChange}
+                    value='heels'
+                    title='Heels'
+                    name='test'
+                />
             </div>
 
         </div>
