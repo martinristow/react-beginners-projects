626ecbace18611d8813cdd45ed5630e81f4c20b5
martinristow <martinristov084@gmail.com>
2025-04-07 13:19:24 +0200

    - Implement react-icons - Styling nav bar

--- E-Commerce-Advance-Filtering/src/Navigation/Nav.jsx
+++ E-Commerce-Advance-Filtering/src/Navigation/Nav.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import {FiHeart} from "react-icons/fi";
+import {AiOutlineShoppingCart, AiOutlineUserAdd} from "react-icons/ai";
 import './Nav.css'
 
 export const Nav = () => {
     return (
-        <div>Nav</div>
+        <nav>
+            <div className='nav-container'>
+                <input
+                    type="text"
+                    className='search-input'
+                    placeholder='Enter your search shoes'/>
+            </div>
+
+            <div className='profile-container'>
+                <a href="#">
+                    <FiHeart className='nav-icons'/>
+                </a>
+
+                <a href="#">
+                    <AiOutlineShoppingCart className='nav-icons'/>
+                </a>
+
+                <a href="#">
+                    <AiOutlineUserAdd className='nav-icons'/>
+                </a>
+            </div>
+        </nav>
     )
 }
