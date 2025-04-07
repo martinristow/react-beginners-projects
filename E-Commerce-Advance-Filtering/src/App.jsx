2d0e9d4ef98d6d5a949c943e5a26da51b9871c9b
martinristow <martinristov084@gmail.com>
2025-04-07 17:36:52 +0200

    Added Sidebar component and Recommended component are moved up one layer

--- E-Commerce-Advance-Filtering/src/App.jsx
+++ E-Commerce-Advance-Filtering/src/App.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import {Nav} from "./Navigation/Nav.jsx";
 import Products from "./Products/Products.jsx";
 import Recommended from "./Recommended/Recommended.jsx";
+import Sidebar from "./SideBar/Sidebar.jsx";
 
 const App = () => {
     return (
         <div>
+            <Sidebar />
             <Nav/>
-            <Products />
             <Recommended/>
+            <Products />
+
         </div>
     )
 }
